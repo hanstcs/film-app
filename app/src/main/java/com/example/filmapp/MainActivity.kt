362903a027649ddfdc6f729d35191ddf61f9f0a6
Hans Simorangkir <simorangkirhans@gmail.com>
2021-08-27 23:14:05 +0700

    Add view binding and dependency injection

--- app/src/main/java/com/example/filmapp/MainActivity.kt
+++ app/src/main/java/com/example/filmapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.filmapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.filmapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private var bindingInst: ActivityMainBinding? = null
+    private val binding get() = bindingInst!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingInst = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        bindingInst = null
     }
 }
